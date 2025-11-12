bc14af89d31af46fcc199aa65c851f8c33ba4965
Vaibhav <bhopalevaibhav5@gmail.com>
2025-11-12 23:55:35 +0530

    Fix project root structure for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    plugins: [react()],
+    base: "/Vaib-tour-expense-splitter/",
     build: {
-	outDir: 'dist'
-    }
-})
+	outDir: 'dist',
+    },
+});
